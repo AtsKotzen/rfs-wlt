41bba763d2c77ca0b10cbfff01cb8fb7b2c403f9
AtsKotzen <atsaudio@gmail.com>
2022-03-04 20:16:44 -0300

    Ajustes de signup

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
 const express = require('express');
 const ethers = require('ethers');
 const Moralis  = require('moralis/node');
-
+const serverUrl = "https://rd3zwgbmyxc7.usemoralis.com:2053/server";
+const appId = "WJpctCjutLfVyuvPYaGLkywcrd5ZRwy7cTmmnJ3u";
+Moralis.start({ serverUrl, appId });
 
 const app = express();
 
-app.get("/importwallet", (req, res) => {
-  let phrase = req.body.phrase;  // get phrase from request
-  let walletAddress = ethers.Wallet.fromMnemonic(phrase); // create wallet from phrase
- 
-  res.send(walletAddress.address);
+app.get("/signup", (req, res) => {
+  const user = new Moralis.User();
+  user.set("username", "Olvra");
+  user.set("password", "12456789");
+  user.set("email", "oi@exple.com");
+   
+  user.signUp().then((user) => {
+    try {
+      res.send(user);
+    }
+    catch(err) {console.log(err)}
+  });  
+;});
+
+app.get("/balances", (req, res) => {  
+  res.send(balances); 
 });
 
-app.get("/balances", (req, res) => {
-  res.send("Get Balances!");
-});
-
-app.get("/transfer", (req, res) => {
-  res.send("Transfer");
+app.get("/transfer", (req, res) => {  
+  res.send("transfer");
 });
 
 app.get("/createwallet", (req, res) => {
     let randomMnemonic = ethers.Wallet.createRandom().mnemonic; // Generate a random mnemonic
     let newphrase = randomMnemonic.phrase;
-    let walletAddress = ethers.Wallet.fromMnemonic(newphrase); // create wallet from phrase
+    let walletAddress = ethers.Wallet.fromMnemonic(newphrase);
+    
+    // Get Private Key from mnemonics
+    let privateKey = walletAddress.privateKey;
+    let privateKeyHex = privateKey.substring(2);
     let payload = {
       "address": walletAddress.address,
-      "phrase": newphrase
+      "phrase": newphrase,
+      "privateKeyHex": privateKeyHex,
+      "privateKey": privateKey      
     }
     res.send(payload);   
 });
